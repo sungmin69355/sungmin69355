5429ababacc2da96897e9e54abb02e69d7487f6d
Cho Sungmin <sungmin69355@naver.com>
2022-01-16 18:01:07 +0900

    Update update_readme.py

--- update_readme.py
+++ update_readme.py
@@ -27,6 +27,8 @@ markdown_text = """
 ![Sungmin's GitHub stats](https://github-readme-stats.vercel.app/api?username=sungmin69355&show_icons=true&theme=radical)[![Sungmin's github stats](https://github-readme-stats.vercel.app/api/top-langs/?username=sungmin69355&show_icons=true&hide_border=true&title_color=004386&icon_color=004386&layout=compact)](https://github.com/sungmin69355)
 
 
+🤩 Latest Blog Post
+
 """
 
 readme_text = f"{markdown_text}{latest_blog_post_list}" 
